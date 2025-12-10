2f7c9cb5947323cd58536226ddb5e2ac50dcc661
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2025-12-10 19:06:08 +0000

    Update update-gamevals.yml
    .
    
    Update update-gamevals.yml
    
    Update update-gamevals.yml
    
    Update update-gamevals.yml
    
    Update update-gamevals.yml
    
    Update parse_gamevals.py
    
    Update update-gamevals.yml

--- src/main/java/rs117/hd/opengl/shader/ModelPassthroughComputeProgram.java
+++
@@ -1,9 +0,0 @@
-package rs117.hd.opengl.shader;
-
-import org.lwjgl.opengl.*;
-
-public class ModelPassthroughComputeProgram extends ShaderProgram {
-	public ModelPassthroughComputeProgram() {
-		super(t -> t.add(GL43C.GL_COMPUTE_SHADER, "comp_unordered.glsl"));
-	}
-}
--- src/main/java/rs117/hd/utils/ShaderRecompile.java
+++
@@ -1,9 +0,0 @@
-package rs117.hd.utils;
-
-import lombok.RequiredArgsConstructor;
-import rs117.hd.opengl.shader.ShaderIncludes;
-
-@RequiredArgsConstructor
-public class ShaderRecompile {
-	public final ShaderIncludes includes;
-}
